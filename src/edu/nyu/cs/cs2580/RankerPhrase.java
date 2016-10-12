dac4e56517b2e05d2c27771b6f6f083240385b5d
Anish Mahesh <anish@Anishs-MacBook-Pro.local>
2016-10-11 21:38:46 -0400

    Added all original files and directories

---
+++ src/edu/nyu/cs/cs2580/RankerPhrase.java
@@ -0,0 +1,34 @@
+package edu.nyu.cs.cs2580;
+
+import edu.nyu.cs.cs2580.QueryHandler.CgiArguments;
+import edu.nyu.cs.cs2580.SearchEngine.Options;
+
+import java.util.ArrayList;
+import java.util.Vector;
+
+/**
+ * @CS2580: Use this template to implement the phrase ranker for HW1.
+ * 
+ * @author congyu
+ * @author fdiaz
+ */
+public class RankerPhrase extends Ranker {
+
+  public RankerPhrase(Options options,
+      CgiArguments arguments, Indexer indexer) {
+    super(options, arguments, indexer);
+    System.out.println("Using Ranker: " + this.getClass().getSimpleName());
+  }
+
+  @Override
+  public Vector<ScoredDocument> runQuery(Query query, int numResults) {
+    Vector<ScoredDocument> all = new Vector<ScoredDocument>();
+    // @CS2580: fill in your code here.
+    return all;
+  }
+
+  public static void main(String args[]) {
+    ArrayList<Integer> a = new ArrayList<Integer>();
+
+  }
+}
