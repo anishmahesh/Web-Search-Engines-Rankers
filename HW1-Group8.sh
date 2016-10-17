816e48110084e2e3d3ef940c4665773020629d1c
anishmahesh <anish.v.mahesh@gmail.com>
2016-10-17 14:41:01 -0400

    Changed all echo statements to printf in the shell script for compatibility with linserv1 machine

--- HW1-Group8.sh
+++ HW1-Group8.sh
@@ -26,46 +26,45 @@ rm -r TestOutputs/* &> /dev/null
 
 
 compile_code () {
-    echo "\nCompiling Java Code\n"
+    printf "\nCompiling Java Code\n"
     {
         javac src/edu/nyu/cs/cs2580/*.java
     } &> /dev/null
     STATUS=$?
     if [ $STATUS -eq 0 ]; then
-        echo "\n\n${green}Java Build Successful${NC}\n\n"
+        printf "\n\n${green}Java Build Successful${NC}\n\n"
     else
-        echo "\n\n${red}Java Build Failed${NC}\n\n"
+        printf "\n\n${red}Java Build Failed${NC}\n\n"
         exit 1
     fi
 }
 
 
 run_indexer () {
-    echo "\nCreating search Index\n"
+    printf "\nCreating search Index\n"
     {
         java -cp src edu.nyu.cs.cs2580.SearchEngine --mode=index --options=conf/engine.conf
     } &> /dev/null
     STATUS=$?
     if [ $STATUS -eq 0 ]; then
-        echo "\n\n${green}Search Index Created${NC}\n\n"
+        printf "\n\n${green}Search Index Created${NC}\n\n"
     else
-        echo "\n\n${red}Index Creation Failed${NC}\n\n"
+        printf "\n\n${red}Index Creation Failed${NC}\n\n"
         exit 1
     fi
 }
 
 
 start_server () {
-    echo "\nStarting Java Search Engine Server\n"
+    printf "\nStarting Java Search Engine Server\n"
     {
         ps aux | grep  25808 | awk '{print $2}' |xargs kill -9
         java -cp src -Xmx512m edu.nyu.cs.cs2580.SearchEngine --mode=serve --port=25808 --options=conf/engine.conf &
+        #storing PID of the server process,to shut it down later
         echo $! > java_pid
     } &> /dev/null
     STATUS=$?
     if [ $STATUS -eq 0 ]; then
-        #storing PID of the server process,to shut it down later
-        echo $JavaServerPid > java_pid.txt
         #waiting until the server has started
         url=http://localhost:25808/
         printf "Loading Index "
@@ -73,9 +72,9 @@ start_server () {
             printf '.'
             sleep 2
         done
-        echo "\n\n${green}Server Started${NC}\n\n"
+        printf "\n\n${green}Server Started${NC}\n\n"
     else
-        echo "\n\n${red}Initialising Server Encountered an error${NC}\n\n"
+        printf "\n\n${red}Initialising Server Encountered an error${NC}\n\n"
         exit 1
     fi
 }
@@ -83,7 +82,7 @@ start_server () {
 
 
 test () {
-    echo "\nStarting Test Case Execution\n"
+    printf "\nStarting Test Case Execution\n"
 
     run_query () {
         q=$1
@@ -114,7 +113,7 @@ test () {
                 n=10
             fi
         done
-        echo "\n"
+        printf "\n\n"
     }
 
     filename="data/queries.tsv"
@@ -122,49 +121,49 @@ test () {
     while read -r line
     do
         query="$line"
-        echo "\n\nQuery : ${blue}$query${NC}"
+        printf "\n\nQuery : ${blue}$query${NC}"
 
-        echo "\nRunning QL Ranker"
+        printf "\nRunning QL Ranker"
         {
             run_query "$query" "ql"
         } >> TestOutputs/hw1.1-ql.tsv  2>&0
 
-        echo "\nRunning Cosine Ranker"
+        printf "\nRunning Cosine Ranker"
         {
             run_query "$query" "cosine"
         } >> TestOutputs/hw1.1-vsm.tsv 2>&0
 
-        echo "\nRunning Phrase Ranker"
+        printf "\nRunning Phrase Ranker"
         {
             run_query "$query" "phrase"
         } >> TestOutputs/hw1.1-phrase.tsv 2>&0
 
-        echo "\nRunning Linear Ranker"
+        printf "\nRunning Linear Ranker"
         {
             run_query "$query" "linear"
         } >> TestOutputs/hw1.2-linear.tsv 2>&0
 
-        echo "\nRunning Numviews Ranker"
+        printf "\nRunning Numviews Ranker"
         {
             run_query "$query" "numviews"
         } >> TestOutputs/hw1.1-numviews.tsv 2>&0
 
-        echo "\nEvaluating vsm ranker"
+        printf "\nEvaluating vsm ranker"
         {
             evaluate "$query" "cosine"
         } >> TestOutputs/hw1.3-vsm.tsv  2>&0
 
-        echo "\nEvaluating ql ranker"
+        printf "\nEvaluating ql ranker"
         {
             evaluate "$query" "ql"
         } >> TestOutputs/hw1.3-ql.tsv  2>&0
 
-        echo "\nEvaluating numviews ranker"
+        printf "\nEvaluating numviews ranker"
         {
             evaluate "$query" "ql"
         } >> TestOutputs/hw1.3-numviews.tsv  2>&0
 
-        echo "\nEvaluating phrase ranker"
+        printf "\nEvaluating phrase ranker"
         {
             evaluate "$query" "phrase"
         } >> TestOutputs/hw1.3-phrase.tsv  2>&0
@@ -174,16 +173,16 @@ test () {
 
 
 shut_server () {
-    echo "\n\nShuting down Java Server\n"
+    printf "\n\nShuting down Java Server\n"
     {
         value=`cat java_pid`
         kill $value
     } &> /dev/null
     STATUS=$?
     if [ $STATUS -eq 0 ]; then
-        echo "\n\n${green}Server Stopped${NC}\n\n"
+        printf "\n\n${green}Server Stopped${NC}\n\n"
     else
-        echo "\n\n${red}Error in shutting down the server${NC}\n\n"
+        printf "\n\n${red}Error in shutting down the server${NC}\n\n"
         exit 1
     fi
     rm java_pid
@@ -191,7 +190,7 @@ shut_server () {
 
 
 usage () {
-    echo "\n\nUsage: `basename $0` \n-- This is a utility script for server commands and test execution\n\nCommands: [-r] [-t] [-s] [--run] [--stop] [--test]\n\nwhere:\n-r\tcompiles code, sets up index and runs the java server\n-t\tcompiles code, starts server, indexes and generates the output cases\n-s\tstops the java server\n\n"
+    printf "\n\nUsage: `basename $0` \n-- This is a utility script for server commands and test execution\n\nCommands: [-r] [-t] [-s] [--run] [--stop] [--test]\n\nwhere:\n-r\tcompiles code, sets up index and runs the java server\n-t\tcompiles code, starts server, indexes and generates the output cases\n-s\tstops the java server\n\n"
 }
 
 
@@ -203,23 +202,23 @@ fi
 while [ "$1" != "" ]; do
     case $1 in
         -r | --run )    shift
-                        echo "\n\n${bold}${yellow}CSGA-2580 - Group 8 - Server Runner${NC}\n\n"
+                        printf "\n\n${bold}${yellow}CSGA-2580 - Group 8 - Server Runner${NC}\n\n"
                         compile_code
                         run_indexer
                         start_server
                         ;;
         -s | --stop )
-                        echo "\n\n${bold}${yellow}CSGA-2580 - Group 8 - Stop Processor${NC}\n\n"
+                        printf "\n\n${bold}${yellow}CSGA-2580 - Group 8 - Stop Processor${NC}\n\n"
                         shut_server
                         ;;
         -t | --test )
-                        echo "\n\n${bold}${yellow}CSGA-2580 - Group 8 - Test Output Files Generator${NC}\n\n"
+                        printf "\n\n${bold}${yellow}CSGA-2580 - Group 8 - Test Output Files Generator${NC}\n\n"
                         compile_code
                         run_indexer
                         start_server
                         test
                         shut_server
-                        echo "\nTest case execution ${green}completed!${NC} Output files generated in ${bold}./TestOutputs${NC}\n"
+                        printf "\nTest case execution ${green}completed!${NC} Output files generated in ${bold}./TestOutputs${NC}\n"
                         ;;
         -h | --help )
                         usage
